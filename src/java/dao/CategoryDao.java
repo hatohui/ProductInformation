9ff3436f89123ec9977498f593cef9e530ef7974
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-02-24 06:42:20 +0700

    init

--- src/java/dao/CategoryDao.java
+++ src/java/dao/CategoryDao.java
@@ -1,14 +1,28 @@
 package dao;
 
 import interfaces.Workable;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.Category;
+import utils.DatabaseInstance;
 
 public class CategoryDao implements Workable<Category> {
 
     @Override
     public List<Category> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Category> list = new ArrayList<>();
+        String query = "SELECT productId, productName, productImage, brief, postedDate, typeId, account, unit, price, discount "
+                + "FROM products;";
+        try {
+            DatabaseInstance.connectToDatabase();
+            List<Category> categories = DatabaseInstance.query(query, Category.class);
+            DatabaseInstance.close();
+            return categories;
+        } catch (SQLException exception) {
+            System.out.println(exception);
+        }
+        return list;
     }
 
     @Override
